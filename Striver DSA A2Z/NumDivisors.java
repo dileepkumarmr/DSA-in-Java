da61c8ac79c0a5de101d38804c195712d7797578
dileepkumarmr <dileepmr35@gmail.com>
2023-11-02 17:31:06 +0530

    ..

--- Striver DSA A2Z/NumDivisors.java
+++ Striver DSA A2Z/NumDivisors.java
@@ -1,9 +1,21 @@
-/**
- * NumDivisors
- */
+
 public class NumDivisors {
 
+    public static void printDivisors(int n) {
+
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+            if (i != n / i) {
+                System.out.print(n / i + " ");
+            }
+        }
+        System.out.println();
+
+    }
+
     public static void main(String[] args) {
-        
+        printDivisors(36);
     }
 }
