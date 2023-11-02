fad24779f5f05111a5aada10d4c02a157cb2f832
dileepkumarmr <dileepmr35@gmail.com>
2023-11-02 19:46:56 +0530

    .

---
+++ Kunal DSA/sieve.java
@@ -0,0 +1,26 @@
+public class sieve {
+
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n + 1];
+        // System.out.println(primes[0]);
+        Sieve(n, primes);
+
+    }
+
+    static void Sieve(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    primes[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+}
