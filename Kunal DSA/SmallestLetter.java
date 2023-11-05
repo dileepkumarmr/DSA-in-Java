d07a6c90ef63b1af5709ff37ade81c7ebddef7f2
dileepkumarmr <dileepmr35@gmail.com>
2023-11-05 13:21:10 +0530

    .

--- Kunal DSA/SmallestLetter.java
+++ Kunal DSA/SmallestLetter.java
@@ -1,25 +1,47 @@
 public class SmallestLetter {
 
     public static char nextGreatestLetter(char[] letters, char target) {
-        int start = 0;
-        int end = letters.length - 1;
+        // int start = 0;
+        // int end = letters.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        // while (start <= end) {
+        // int mid = start + (end - start) / 2;
 
-            if (target < letters[mid]) {
-                end = mid - 1;
+        // if (target < letters[mid]) {
+        // end = mid - 1;
+        // } else {
+        // start = mid + 1;
+        // }
+
+        // }
+        // return letters[start % letters.length];
+
+        char res = 'a';
+        int low = 0;
+        int high = letters.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (letters[mid] > target) {
+                res = letters[mid];
+                high = mid - 1;
             } else {
-                start = mid + 1;
+                low = mid + 1;
             }
-
         }
-        return letters[start % letters.length];
+
+        if (res <= target) {
+            return letters[0];
+        }
+        return res;
+
     }
 
     public static void main(String[] args) {
-        char[] letters = { 'c', 'f', 'g' };
-        char target = 'a';
+        // char[] letters = { 'c', 'f', 'g' };
+        char[] letters = { 'x',  'y' };
+
+        char target = 'z';
 
         System.out.println(nextGreatestLetter(letters, target));
     }
