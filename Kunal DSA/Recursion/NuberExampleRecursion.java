fbd996193bb2780852aaf495a2d6c1ac3c610218
dileepkumarmr <dileepmr35@gmail.com>
2023-11-14 18:08:57 +0530

    .

---
+++ Kunal DSA/Recursion/NuberExampleRecursion.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class NuberExampleRecursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    public static void print(int n) {
+
+        if (n == 5) {
+            return;
+        }
+        System.out.println(n);
+        print(n + 1);
+    }
+}
