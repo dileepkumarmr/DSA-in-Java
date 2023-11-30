238c1eb6beb871826650c21ec17371b5f4e94364
dileepkumarmr <dileepmr35@gmail.com>
2023-11-30 09:43:35 +0530

    .

--- Kunal DSA/Recursion/Fibo.java
+++ Kunal DSA/Recursion/Fibo.java
@@ -1,19 +1,22 @@
-
 package Recursion;
 
-/**
- * Fibo
- */
 public class Fibo {
 
+    public static int fiboFormula(int n) {
+        return (int) ((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
+    }
+
     static int fibo(int n) {
-        if (n<2) {
+        if (n < 2) {
             return n;
         }
         return fibo(n - 1) + fibo(n - 2);
     }
 
     public static void main(String[] args) {
-        System.out.println(fibo(40));
+        // System.out.println(fibo(40));
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fiboFormula(i));
+        }
     }
 }
