e9b9ad20a4ebf40c6290b15f1a2782f8a937169c
dileepkumarmr <dileepmr35@gmail.com>
2023-11-30 14:16:19 +0530

    .

--- Kunal DSA/Sorting/BubbleSort.java
+++ Kunal DSA/Sorting/BubbleSort.java
@@ -1,5 +1,60 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
-    
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 1, 2, 3 };
+        // bubble(arr);
+
+        // System.out.println(getMaxIndex(arr, 0, 4));
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+            System.out.println(maxIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    public static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    public static void bubble(int[] arr) {
+
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
