76946a03cdbe5f6532f46da145f50502dd796343
dileepkumarmr <dileepmr35@gmail.com>
2023-12-02 12:36:12 +0530

    .

--- Kunal DSA/Sorting/BubbleSort.java
+++ Kunal DSA/Sorting/BubbleSort.java
@@ -4,12 +4,24 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = { 4, 5, 1, 2, 3 };
+        int[] arr = { 5, 3, 4, 1, 2 };
         // bubble(arr);
 
         // System.out.println(getMaxIndex(arr, 0, 4));
-        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     public static void selection(int[] arr) {
