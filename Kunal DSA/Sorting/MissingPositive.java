8a801014a8e950a44027ba92de6edcea7ea2ae71
dileepkumarmr <dileepmr35@gmail.com>
2023-12-03 20:26:14 +0530

    .

--- Kunal DSA/Sorting/MissingPositive.java
+++ Kunal DSA/Sorting/MissingPositive.java
@@ -1,40 +1,34 @@
 package Sorting;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class FindDuplicateArray {
+public class MissingPositive {
     public static void main(String[] args) {
-        int[] nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
-        System.out.println(findDuplicateArray(nums));
+        int[] arr = { 1, 2, 0 };
+        System.out.println(firstMissingPositive(arr));
     }
 
-    public static List<Integer> findDuplicateArray(int[] nums) {
-
+    public static int firstMissingPositive(int[] nums) {
         int i = 0;
         while (i < nums.length) {
+
             int correct = nums[i] - 1;
-            if (nums[i] != nums[correct]) {
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
                 swap(nums, i, correct);
             } else {
                 i++;
             }
         }
 
-        List<Integer> ans = new ArrayList<>();
-
         for (int index = 0; index < nums.length; index++) {
             if (nums[index] != index + 1) {
-                ans.add(nums[index]);
+                return index + 1;
             }
         }
-        return ans;
+        return nums.length + 1;
     }
 
-    static void swap(int[] arr, int first, int second) {
+    public static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
     }
-
 }
