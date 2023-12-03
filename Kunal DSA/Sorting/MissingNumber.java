8fe086ad1d3c84c7d452d2332161ad2084382374
dileepkumarmr <dileepmr35@gmail.com>
2023-12-03 11:45:31 +0530

    .

---
+++ Kunal DSA/Sorting/MissingNumber.java
@@ -0,0 +1,37 @@
+package Sorting;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 0, 2, 1 };
+        missingNumber(arr);
+        System.out.println(missingNumber(arr));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index) {
+                return index;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}
