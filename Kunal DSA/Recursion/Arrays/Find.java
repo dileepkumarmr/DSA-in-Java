c69652de9aea32f77adf4d88e4588942c2f83fb0
dileepkumarmr <dileepmr35@gmail.com>
2023-12-08 16:44:39 +0530

    .

--- Kunal DSA/Recursion/Arrays/Find.java
+++ Kunal DSA/Recursion/Arrays/Find.java
@@ -1,11 +1,15 @@
 package Recursion.Arrays;
 
+import java.util.ArrayList;
+
 public class Find {
     public static void main(String[] args) {
-        int[] arr = { 2, 3, 1, 4, 5 };
+        int[] arr = { 2, 3, 1, 4, 4, 5 };
         System.out.println(find(arr, 4, 0));
         System.out.println(findIndex(arr, 4, 0));
         System.out.println(findIndexLast(arr, 4, arr.length - 1));
+        findAllIndex(arr, 4, 0);
+        System.out.println(list);
     }
 
     static boolean find(int[] arr, int target, int index) {
@@ -38,4 +42,29 @@ public class Find {
         }
     }
 
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
+        }
+
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+
+    }
+
+    static ArrayList findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, list);
+
+    }
 }
