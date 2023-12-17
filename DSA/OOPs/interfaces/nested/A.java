3ba6353a51ef9bb11114a4d16f22e123a6c27c61
dileepkumarmr <dileepmr35@gmail.com>
2023-12-17 13:43:13 +0530

    .

--- DSA/OOPs/interfaces/nested/A.java
+++ DSA/OOPs/interfaces/nested/A.java
@@ -1,5 +1,16 @@
 package OOPs.interfaces.nested;
 
 public class A {
+//    NestedInterface
+    public interface NestedInterface {
+    boolean isOdd(int num);
+    }
 
+}
+class B implements A.NestedInterface {
+
+    @Override
+    public boolean isOdd(int num) {
+        return (num & 1)==1;
+    }
 }
