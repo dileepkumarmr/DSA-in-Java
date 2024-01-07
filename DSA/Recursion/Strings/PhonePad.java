3674ef610ac4f16bd554744cd72e9ffc766b9787
dileepkumarmr <dileepmr35@gmail.com>
2024-01-07 19:50:33 +0530

    .

--- DSA/Recursion/Strings/PhonePad.java
+++ DSA/Recursion/Strings/PhonePad.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class PhonePad {
     public static void main(String[] args) {
 //        pad("", "12");
-//        System.out.println(padRet("", "12"));
-        System.out.println(padCount("", "12"));
+        System.out.println(padRet("", "12"));
+//        System.out.println(padCount("", "12"));
     }
 
     static void pad(String p, String up) {
@@ -25,23 +25,18 @@ public class PhonePad {
     }
 
     static ArrayList<String> padRet(String p, String up) {
-
         if (up.isEmpty()) {
 //            System.out.println(p);
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
-
         int digit = up.charAt(0) - '0';
-
         ArrayList<String> list = new ArrayList<>();
-
         for (int i = (digit - 1) * 3; i < digit * 3; i++) {
             char ch = (char) ('a' + i);
             list.addAll(padRet(p + ch, up.substring(1)));
         }
-
         return list;
     }
 
